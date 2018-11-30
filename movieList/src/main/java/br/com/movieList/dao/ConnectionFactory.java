ffdbc2ba49c6f8a2b771e0977575a0ccd61fe661
Unknown <andreluis2006@outlook.com>
2018-11-30 21:07:01 -0200

    Alterações no CF

--- movieList/src/main/java/br/com/movieList/dao/ConnectionFactory.java
+++ movieList/src/main/java/br/com/movieList/dao/ConnectionFactory.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
-	private static final  String stringConexao = "jdbc:mysql://localhost:3306/easy_task?useTimezone=true&serverTimezone=UTC&autoReconnect=true&useSSL=false";
+	private static final  String stringConexao = "jdbc:mysql://localhost:3306/movie_list?useTimezone=true&serverTimezone=UTC&autoReconnect=true&useSSL=false";
     private static final String usuarioBD = "root";
     private static final String senhaBD = "meteoro";
 
