422909a6cd0cc784fd31c225d9a66dcbfd87fedd
Fernando Sutter <fernando.sutter1@gmail.com>
2018-11-30 21:18:33 -0200

    Alterações MovieDAO

--- movieList/src/main/java/br/com/movieList/dao/MovieDao.java
+++ movieList/src/main/java/br/com/movieList/dao/MovieDao.java
@@ -1,12 +1,28 @@
 package br.com.movieList.dao;
 
-public class MovieDao {
-<<<<<<< HEAD
-       
-	
-//
-	//
-=======
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+ import br.com.movieList.movie.Movie;
 
->>>>>>> master
+public class MovieDao {
+	public void insertMovie(Movie movie) {
+        try {
+           
+        	// Cria a conexãoo com o banco de dados
+            MovieDao moviedDAO = new MovieDao();
+             Connection conn = (new ConnectionFactory()).getConnection();
+            PreparedStatement p =
+                    conn.prepareStatement("insert into movies(movie_name, is_watched) values(?, ?) ");
+            p.setString(1, movie.getName());
+            p.setString(2, movie.getWatched());
+            p.execute();
+            p.close();
+            conn.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+}
+	}
+	
+	
+	
 }
