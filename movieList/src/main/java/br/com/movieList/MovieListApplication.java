b96bdac1c2432e4d0f4edece44dea250f8d2dd4f
Fernando Sutter <fernando.sutter1@gmail.com>
2018-11-30 20:09:07 -0200

    Inicio do Projeto

---
+++ movieList/src/main/java/br/com/movieList/MovieListApplication.java
@@ -0,0 +1,12 @@
+package br.com.movieList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListApplication.class, args);
+	}
+}
